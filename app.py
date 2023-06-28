7bca08a37fefdff8f3d501bc36e51a2f82160f82
Achal Mahale <achalmahale@gmail.com>
2023-06-28 20:02:16 +0530

    Added GET by name

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from flask_restful import Api, Resource
 from pymongo import MongoClient
 from bson.objectid import ObjectId
@@ -7,28 +7,32 @@ import os
 
 load_dotenv()
 
+# Creating a Flask app instance
 flaskAppInstance = Flask(__name__)
 api = Api(flaskAppInstance)
 
+# Connecting to MongoDB
 username = os.getenv('MONGODB_USERNAME')
 password = os.getenv('MONGODB_PASSWORD')
 cluster_address = os.getenv('MONGODB_CLUSTER_ADDRESS')
 database = os.getenv('MONGODB_DATABASE')
-
 connection_string = f'mongodb+srv://{username}:{password}@{database}.{cluster_address}/?retryWrites=true&w=majority'
 client = MongoClient(connection_string)
+
+# Creating a database and collection instance
 db = client['game_list']
 collection = db['games']
-print(connection_string)
 
 #Create a class using the Resource class from flask_restful
 class Game(Resource):
 
+    # GET
     def get(self):
         data = list(collection.find())
         serialized_data = [{**doc, '_id': str(doc['_id'])} for doc in data]
         return jsonify(serialized_data)
-
+    
+    # POST
     def post(self):
         data = request.get_json()
         result = collection.insert_one(data)
@@ -38,10 +42,24 @@ class Game(Resource):
             'message': "Data Inserted Successfully",
             '_id': inserted_id
         }
+
+class SearchGame(Resource):
+    @flaskAppInstance.route("/search")
+    def search_games():
+        name = request.args.get("name")  # Get the name parameter from the query string
+        if name:
+            data = list(collection.find({"name": name}))
+            serialized_data = [{**doc, "_id": str(doc["_id"])} for doc in data]
+            return jsonify(serialized_data)
+        else:
+            return {"message": "Please provide a name parameter"}, 400
+
+
     
 # Create another class using the Resource class from flask_restful
 class GameList(Resource):
 
+    # GET by ID
     def get(self, game_id):
         try:
             data = collection.find_one({'_id': ObjectId(game_id)})
@@ -53,6 +71,7 @@ class GameList(Resource):
         except (TypeError, ValueError):
             return {'message': 'Invalid game ID'}, 400
         
+    # PUT by ID        
     def put(self, game_id):
         try:
             data = request.get_json()
@@ -69,7 +88,7 @@ class GameList(Resource):
         except (TypeError, ValueError):
             return {'message': 'Invalid game ID'}, 400
         
-
+    # DELETE by ID
     def delete(self, game_id):
         try:
             collection.delete_one({'_id': ObjectId(game_id)})
@@ -77,13 +96,31 @@ class GameList(Resource):
         except (TypeError, ValueError):
             return {'message': 'Invalid game ID'}, 400
         
+    # PATCH by ID
+    def patch(self, game_id):
+        try:
+            data = request.get_json()
+            updated_data = collection.find_one_and_update(
+                {'_id': ObjectId(game_id)},
+                {'$set': data},
+                return_document=True
+            )
+            if updated_data:
+                serialized_data = {**updated_data, '_id': str(updated_data['_id'])}
+                return jsonify(serialized_data)
+            else:
+                return {'message': 'Game not found'}, 404
+        except (TypeError, ValueError):
+            return {'message': 'Invalid game ID'}, 400
+    
 
-
+# Adding the URL endpoints to the API
 restServerInstance = Api(flaskAppInstance)
 restServerInstance.add_resource(Game,"/games")
 restServerInstance.add_resource(GameList,"/games/<string:game_id>")
+restServerInstance.add_resource(SearchGame,"/search>")
+
 
 
 if __name__ == '__main__':
     flaskAppInstance.run(debug=True, host="0.0.0.0", port=5000)
-
